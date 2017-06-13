c09bef4e124e2211c5774004a886a1e47a17f787
Jan Jaap van Deursen <j.j.m.vandeursen@gmail.com>
2017-06-13 09:25:33 +0200

    Tabs to spaces

--- loadJS.js
+++ loadJS.js
@@ -6,64 +6,64 @@
  * @return {Element} script - reference to the create script element
  */
 const loadJS = (options) => {
-	if (!options.src) {
-		throw new Error('[loadJS] \'src\' is missing in options object');
-	}
+    if (!options.src) {
+        throw new Error('[loadJS] \'src\' is missing in options object');
+    }
 
-	// default settings
-	// will be merged with element data attibutes and the options object
-	const defaultSettings = {
+    // default settings
+    // will be merged with element data attibutes and the options object
+    const defaultSettings = {
         /**
          * Setting this to false will force the browser to (asynchronously, oh the irony) load
          * scripts in the order they are requested.
          * @type {Boolean}
          */
-		async: true,
+        async: true,
 
         /**
          * @see https://developer.mozilla.org/en-US/docs/Web/Security/Subresource_Integrity
          * @type {String}
          */
-		integrity: '',
+        integrity: '',
 
         /**
          * @type {String}
          */
-		type: 'text/javascript',
+        type: 'text/javascript',
 
         /**
          * @see https://developer.mozilla.org/en-US/docs/Web/HTML/Element/script#attr-crossorigin
          * @type {String}
          */
-		crossOrigin: null,
+        crossOrigin: null,
 
         /**
          * @type {Function}
          */
-		onload: (() => { }),
+        onload: (() => { }),
 
         /**
          * @type {Function}
          */
-		onerror: (() => { }),
-	};
+        onerror: (() => { }),
+    };
 
-	const settings = Object.assign(defaultSettings, options);
-	const ref = document.getElementsByTagName('script')[0];
-	const script = document.createElement('script');
-	const settingsKeys = Object.keys(settings);
-	const settingsLength = settingsKeys.length;
+    const settings = Object.assign(defaultSettings, options);
+    const ref = document.getElementsByTagName('script')[0];
+    const script = document.createElement('script');
+    const settingsKeys = Object.keys(settings);
+    const settingsLength = settingsKeys.length;
 
-	for (let i = 0; i < settingsLength; i += 1) {
-		script[settingsKeys[i]] = settings[settingsKeys[i]];
-	}
+    for (let i = 0; i < settingsLength; i += 1) {
+        script[settingsKeys[i]] = settings[settingsKeys[i]];
+    }
 
-	ref.parentNode.insertBefore(script, ref);
+    ref.parentNode.insertBefore(script, ref);
 
-	script.onload = settings.onload;
-	script.onerror = settings.onerror;
+    script.onload = settings.onload;
+    script.onerror = settings.onerror;
 
-	return script;
+    return script;
 };
 
 window.loadJS = loadJS;
